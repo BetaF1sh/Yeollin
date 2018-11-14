7e9d27b5e9e1c814dc3d396f3ed105fbb7c24c0b
Muhun Kim <iam@muhun.kim>
2018-11-14 23:39:43 +0900

    Add semantic ui and scss

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import TestWrapper from '@/components/TestWrapper'
 
-Vue.use(Router)
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css'
+
+Vue.use(Router);
+Vue.use(SuiVue);
 
 export default new Router({
   routes: [
